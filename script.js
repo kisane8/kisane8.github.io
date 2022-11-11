5792885716b24667a83663555ada6a676ce0f4ac
kisane8 <65173854+kisane8@users.noreply.github.com>
2022-11-11 12:56:23 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,88 @@
+function yugo() {
+    document.getElementById("character-box").style.backgroundImage = "url('/ressource/img/bg/Yugo.jpg')";
+    var audio = document.getElementById("audio");
+    audio.src = "/ressource/ost/Bloody Roar 2 OST Theme of Yugo.mp3";
+    audio.load();
+    audio.play();
+}
+
+function uriko() {
+    
+    document.getElementById("character-box").style.backgroundImage = "url('/ressource/img/bg/Uriko.jpg')";
+    var audio = document.getElementById("audio");
+    audio.src = "/ressource/ost/Bloody Roar 2 OST Theme of Uriko.mp3";
+    audio.load();
+    audio.play();
+}
+
+function stun() {
+    document.getElementById("character-box").style.backgroundImage = "url('/ressource/img/bg/Stun.jpg')";
+    var audio = document.getElementById("audio");
+    audio.src = "/ressource/ost/Bloody Roar 2 OST Theme of Stun.mp3";
+    audio.load();
+    audio.play();
+}
+
+function long() {
+    document.getElementById("character-box").style.backgroundImage = "url('/ressource/img/bg/Long.jpg')";
+    var audio = document.getElementById("audio");
+    audio.src = "/ressource/ost/Bloody Roar 2 OST Theme of Long.mp3";
+    audio.load();
+    audio.play();
+}
+
+function jenny() {
+    document.getElementById("character-box").style.backgroundImage = "url('/ressource/img/bg/Jenny.jpg')";
+    var audio = document.getElementById("audio");
+    audio.src = "/ressource/ost/Bloody Roar 2 OST Theme of Jenny.mp3";
+    audio.load();
+    audio.play();
+}
+
+function busuzima() {
+    document.getElementById("character-box").style.backgroundImage = "url('/ressource/img/bg/Busuzima.jpg')";
+    var audio = document.getElementById("audio");
+    audio.src = "/ressource/ost/Bloody Roar 2 OST Theme of Busuzima.mp3";
+    audio.load();
+    audio.play();
+}
+
+function bakuryu() {
+    document.getElementById("character-box").style.backgroundImage = "url('/ressource/img/bg/Bakuryu.jpg')";
+    var audio = document.getElementById("audio");
+    audio.src = "/ressource/ost/Bloody Roar 2 OST Theme of Bakuryu.mp3";
+    audio.load();
+    audio.play();
+}
+
+function alice() {
+    document.getElementById("character-box").style.backgroundImage = "url('/ressource/img/bg/Alice.jpg')";
+    var audio = document.getElementById("audio");
+    audio.src = "/ressource/ost/Bloody Roar 2 OST Theme of Alice.mp3";
+    audio.load();
+    audio.play();
+}
+
+function shina() {
+    document.getElementById("character-box").style.backgroundImage = "url('/ressource/img/bg/Shina.jpg')";
+    var audio = document.getElementById("audio");
+    audio.src = "/ressource/ost/Bloody Roar 2 OST Theme of Shina.mp3";
+    audio.load();
+    audio.play();
+}
+
+function gado() {
+    document.getElementById("character-box").style.backgroundImage = "url('/ressource/img/bg/Gado.png')";
+    var audio = document.getElementById("audio");
+    audio.src = "/ressource/ost/Bloody Roar 2 OST Theme of Gado.mp3";
+    audio.load();
+    audio.play();
+}
+
+function shenlong() {
+    document.getElementById("character-box").style.backgroundImage = "url('/ressource/img/bg/Shenlong.png')";
+    var audio = document.getElementById("audio");
+    audio.src = "/ressource/ost/Bloody Roar 2 OST Theme of Shenlong.mp3";
+    audio.load();
+    audio.play();
+}
